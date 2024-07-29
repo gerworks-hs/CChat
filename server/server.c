9c47a6b82c8218a5798e3dce71989d7bb42c69da
itsgerliz <gerworks.hs@gmail.com>
2024-07-29 19:02:08 +0200

    Progress made on the server side

--- server/server.c
+++ server/server.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#define PORT 2135
 
 int main(int argc, char** argv) {
-	int socket_fd = socket(AF_INET, SOCK_STREAM, 0); //Socket FD
+
+	const int server_socket_fd = socket(AF_INET, SOCK_STREAM, 0); //Creating the server socket file descriptor and saving it into socket_fd
+	const struct in_addr *loopback_addr = malloc(4); //Allocate 4 bytes (32bit) pointer to save the in_addr struct in network byte order
 	
-	int local_address;
-	inet_pton(AF_INET, "127.0.0.1", local_address);	
+	inet_pton(AF_INET, "127.0.0.1", loopback_addr); //Convert loopback address to network byte order and send it to loopback_addr
 
-	struct sockaddr_in server_address { //Endpoint (server)
-		AF_INET,
-		htons(PORT),
-		local_address	
+	const struct sockaddr_in server_address {
+		AF_INET
+		htons(33); //Convert port into network byte order
+		*loopback_addr //Retrieve the in_addr struct saved in this pointer
 	}
- 
-	bind(socket_fd, server_address, sizeof(server_address));	
 
-	close(socket_fd);	
+	bind(server_socket_fd, &server_address, sizeof(server_address));
+
+	listen(server_socket_fd, 1);
 
 	return 0;	
 }
