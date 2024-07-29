a6a28f31e204cc9073010817ae3b512a4dcd182d
itsgerliz <gerworks.hs@gmail.com>
2024-07-29 21:19:49 +0200

    Server side quite ready, no error handling, not for production

--- server/server.c
+++ server/server.c
@@ -14,9 +14,21 @@ int main(int argc, char** argv) {
 
 	bind(server_socket_fd, (const struct sockaddr *)&server_address, sizeof(server_address)); //Binding server address structure to local socket
 
+	fprintf(stdout, "Server endpoint socket created and binded\n");
+
 	listen(server_socket_fd, 1); //Start listening for connections on the socket
 
-	accept(server_socket_fd, NULL, NULL);
+	fprintf(stdout, "Server listening on socket\n");
 
+	const int client_inc_socket = accept(server_socket_fd, NULL, NULL); //Accept and stablish connection with incoming client
+	
+	fprintf(stdout, "Connection stablished with client\n");
+
+	send(client_inc_socket, "Hello client", sizeof("Hello client"), 0; //Send client hello to the client
+	
+	fprintf(stdout, "We have sent client hello to the client\n");
+
+	close(client_inc_socket); //Close the new incoming client socket
+	close(server_socket_fd); //Close the socket (via its file descriptor)
 	return 0;	
 }
