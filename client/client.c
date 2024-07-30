8495d3dbb47f94fec79f2cf07e4e81a0c18e3299
itsgerliz <gerworks.hs@gmail.com>
2024-07-30 13:54:29 +0200

    Client seems finished

--- client/client.c
+++ client/client.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
 int main(int argc, char **argv) {
+	const int client_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(33);
+	inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+
+	connect(client_socket_fd, (const struct sockaddr *)&server_address, sizeof(server_address));
+	
+	fprintf(stdout, "Connection stablished\n");
+
+	char buffer[13];	
+
+	recv(client_socket_fd, &buffer, sizeof(buffer), 0);
+
+	fprintf(stdout, "Message received -> %s\n", buffer);
+
+	close(client_socket_fd);
 	return 0;
 }
