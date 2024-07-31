224faf584f1d3d86fcb163e3ee961a30e0d4a02c
itsgerliz <gerworks.hs@gmail.com>
2024-07-31 14:28:17 +0200

    Fix

--- v2/client.c
+++ v2/client.c
@@ -8,6 +8,12 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Usage is -> %s <port>", argv[0]);
 		return 1;
 	}
+	long localPort = strtol(argv[1], NULL, 10); //Convert port in argv to long using strtol
+	if (localPort < 1 || localPort > 65535) {
+		fprintf(stderr, "The port is invalid or out of range\n");
+		return 1;
+	}
+	fprintf(stdout, "Server will listen on port %lu\n", localPort);
 	const int clientSock_fd = socket(AF_INET, SOCK_STREAM, 0); //Create client socket and check for errors
 	if (clientSock_fd == -1) {
 		fprintf(stderr, "Failed to create client socket, aborting...\n");
@@ -16,10 +22,10 @@ int main(int argc, char **argv) {
 	fprintf(stdout, "Client local socket created\n");
 	struct sockaddr_in server_address; //Define server address
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(argv[1]);
+	server_address.sin_port = htons(localPort);
 	inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
 	
-	if (connect(clientSock_fd, &server_address, sizeof(server_address)) != 0) {
+	if (connect(clientSock_fd, (const struct sockaddr *)&server_address, sizeof(server_address)) != 0) {
 		fprintf(stderr, "Error when trying to connect to the server, aborting...\n");
 		close(clientSock_fd);
 		return 1;
